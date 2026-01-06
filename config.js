bf1881d9bb0c4ff75ead8cdb90a3553b1c8774b4
DamzOfficiall <bind.max.1tw.2@gmail.com>
2026-01-06 23:13:06 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tokens: "8035676680:AAG_stsArECN9CD3EHJmiybEmo9ZPkp3YQ4",  // Masukin Bot token kamu
+  owners: "7870295166", // Masukin ID Telegram kamu
+  port: "2000", // Masukin Port panel kamu 
+  ipvps: "http://dhanzzprib.strikedigital.my.id" // Masukin IP vps kamu atau domain panel kamu yg asalnya ( https://AiiSigma.id ) menjadi ( http://AiiSigma.id )
+};
+
+/* WAJIB BACA !!!
+ cari di index.js halaman 1995-1996 di situ masukin bot token sama id tele lu.
+
+ emngnya knpa bang?
+ lu buka aja dulu. di sana udah gw kasi penjelasannya
+*/
